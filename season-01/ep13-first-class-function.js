38fc2b10e3ea6d00b1b03d9a33c6363efd02dcb5
Kalanithimaran P <108470828+kalanithimaran@users.noreply.github.com>
2024-11-26 19:31:24 +0530

    Update ep13-first-class-function.js

--- season-01/ep13-first-class-function.js
+++ season-01/ep13-first-class-function.js
@@ -1,4 +1,4 @@
-// Function Statement
+// Function Statement and Function Declaration
 function a() {
     console.log('Function Statement');
 }
@@ -6,10 +6,16 @@ function a() {
 var b = function () {
     console.log('Function Expression');
 }
+
+var c = function xyz() {
+    console.log('Function Expression');
+    // here xyz is called as xyz inside this function will be output render;
+    // outside xyz is not declared error.
+}
+c(); // Function Expression
+xyz(); // Reference Error
 /* Assigning a function to a variable is called a function expression. */
 /* Difference between both is **Hoisting**. */
- 
-//  Function Declaration
 
 // Anonymous Function
 
@@ -18,5 +24,8 @@ var b = function () {
 // Difference between Parameter Vs Argument
 
 // First Class Function
+// a function takes a function as a parameter and returns a function called as a firstClassFunction
+// First class citizens === First Class Function
 
 // Arrow Function
+// ()=>{};
